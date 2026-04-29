da3e0409df950292dd92239d7c8127d3ccf79a4b
keisyansra <keisya.naulia@gmail.com>
2026-04-29 20:21:03 +0700

    Jobsheet 7: API Routes

--- src/pages/produk/index.tsx
+++ src/pages/produk/index.tsx
@@ -1,18 +1,62 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import ProdukView from "../views/produk";
+// import ProdukView from "../views/produk";
 
-const Produk = () => {
-  const [isLogin] = useState(true); // ini diubah sementari yang sebelumnya false
-  const { push } = useRouter();
+type ProductType = {
+  id: string;
+  name: string;
+  price: number;
+  size: string;
+  category: string;
+}
+
+const kategori = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = () => {
+    fetch("/api/produk")
+      .then((response) => response.json())
+      .then((responsedata) => {
+        setProducts(responsedata.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching produk:", error);
+      });
+  };
 
   useEffect(() => {
-    if (!isLogin) {
-      push("/auth/login");
-    }
+    fetchProducts();
   }, []);
 
-  return <ProdukView />;
+  return (
+    <div>
+      <h1>Daftar Produk</h1>
+      <button onClick={fetchProducts}>Refresh Data</button>
+      {products.map((product:ProductType) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <p>Harga: {product.price}</p>
+          <p>Ukuran: {product.size}</p>
+          <p>Kategori: {product.category}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default Produk;
+export default kategori;
+
+// const Produk = () => {
+//   const [isLogin] = useState(true); // ini diubah sementari yang sebelumnya false
+//   const { push } = useRouter();
+
+//   useEffect(() => {
+//     if (!isLogin) {
+//       push("/auth/login");
+//     }
+//   }, []);
+
+//   return <ProdukView />;
+// };
+
+// export default Produk;
